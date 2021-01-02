978801f8f639e35e5df67d0319ecbe666356db98
NawafSwe <nawaf.jr9@hotmail.com>
2021-01-02 21:21:00 +0300

    Initial Commit

---
+++ mediaPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mediaPlayer
+//
+//  Created by Nawaf B Al sharqi on 02/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
